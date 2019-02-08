1fd571a842d71eae7fdb072636f9572068355448
Nick Freeman <nick@nikulis.io>
2019-02-08 00:39:56 -0500

    Fix: ensure user is logged in to see own profile page

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,5 +1,7 @@
 import Router from 'koa-router';
 import { getRepository } from 'typeorm';
+
+import { assertRole } from '../authn';
 import User from '../models/User';
 
 const router = new Router();
@@ -7,6 +9,8 @@ const router = new Router();
 const Users = getRepository(User);
 
 router.get('/profile', async ctx => {
+  assertRole(ctx);
+
   const user = await Users.findOne({ where: { id: ctx.state.authUser.id } });
   await ctx.render('profile', { user });
 });
