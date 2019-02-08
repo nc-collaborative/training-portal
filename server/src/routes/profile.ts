90d8c693f486dfcd691f0317a162f0ff843ebbe0
Nick Freeman <nick@nikulis.io>
2019-02-08 08:31:55 -0500

    Add completed trainings to own user profile page

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -11,8 +11,30 @@ const Users = getRepository(User);
 router.get('/profile', async ctx => {
   assertRole(ctx);
 
-  const user = await Users.findOne({ where: { id: ctx.state.authUser.id } });
-  await ctx.render('profile', { user });
+  const user = await Users.findOne({
+    where: { id: ctx.state.authUser.id },
+    relations: ['attempts'],
+  });
+
+  const uniqueTrainingIds = Array.from(
+    new Set(user!.attempts.map(a => a.trainingVersion.training.id)),
+  );
+  const uniqueTrainings = uniqueTrainingIds.map(
+    tid =>
+      user!.attempts.find(a => a.trainingVersion.training.id == tid)!
+        .trainingVersion.training,
+  );
+
+  const trainingEntries: any[] = [];
+
+  uniqueTrainings.forEach(t => {
+    let lastDate = user!.attempts
+      .filter(a => a.trainingVersion.training.id == t.id)
+      .reduce((d, a) => (a.updatedOn > d ? a.updatedOn : d), new Date(0));
+    trainingEntries.push({ ...t, lastDate });
+  });
+
+  await ctx.render('profile', { user, trainings: trainingEntries });
 });
 
 export default router;
