777ae04382a228a4edaa2dd000a1a2bbccd88648
Nick Freeman <nick@nikulis.io>
2019-02-11 13:05:03 -0500

    Update user profile page
    Don't list completed trainings on user profile; should be listed
    separately on learner dashboard

--- server/src/routes/users/profile.ts
+++ server/src/routes/users/profile.ts
@@ -17,30 +17,7 @@ router.use(async (ctx, next) => {
 });
 
 router.get('/profile', async ctx => {
-  const user = await Users.findOne({
-    where: { id: ctx.state.authUser.id },
-    relations: ['attempts'],
-  });
-
-  const uniqueTrainingIds = Array.from(
-    new Set(user!.attempts.map(a => a.trainingVersion.training.id)),
-  );
-  const uniqueTrainings = uniqueTrainingIds.map(
-    tid =>
-      user!.attempts.find(a => a.trainingVersion.training.id == tid)!
-        .trainingVersion.training,
-  );
-
-  const trainingEntries: any[] = [];
-
-  uniqueTrainings.forEach(t => {
-    let lastDate = user!.attempts
-      .filter(a => a.trainingVersion.training.id == t.id)
-      .reduce((d, a) => (a.updatedOn > d ? a.updatedOn : d), new Date(0));
-    trainingEntries.push({ ...t, lastDate });
-  });
-
-  await ctx.render('users/profile', { user, trainings: trainingEntries });
+  await ctx.render('users/profile', { user: ctx.state.authUser });
 });
 
 router.get('/profile/edit', async ctx => {
